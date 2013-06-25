267262a75644d052cc442272123ecb71d064f308
Michał Derkacz <michal@Lnet.pl>
2013-06-25 13:46:28 +0200

    Intermediate commit

--- hdc/bitbang.go
+++ hdc/bitbang.go
@@ -10,18 +10,20 @@ import (
 // FTDI USB chips in bitbang mode).
 //
 // It writes 3 bytes for one nibble to the provided io.Writer:
-// - first:  with E bit unset, need >= 140 ns
-// - second: with E bit set,   need >= 450 ns
+// - first:  with E bit unset, need >= 40 ns
+// - second: with E bit set,   need >= 230 ns
 // - thrid:  with E bit unset, need >= 10 ns
-// Full E cycle need >= 1000 ns.
+// Full E cycle need >= 500 ns.
+// Baudrate that satisfy all this time constrains: 1 B / 230 ns = 4347826 B/s
 //
-// Additionaly Bitbang provides methods to controll AUX and R/W bits (if you want to
-// use R/W bit as second AUX, the real R/W pin should be connected to the VSS).
+// Additionaly Bitbang provides methods to controll AUX and R/W bits (if you
+// want to use R/W bit as second AUX, the real R/W pin should be connected to
+// the VSS).
 type Bitbang struct {
 	w          io.Writer
 	e, rw, aux byte
 	a          byte
-	buf        [80 * 2 * 3]byte
+	buf        [6]byte
 }
 
 // NewBitbang re
@@ -41,6 +43,11 @@ func (o *Bitbang) SetMapping(e, rw, aux byte) {
 }
 
 func (o *Bitbang) Write(data []byte) (int, error) {
+	if len(data) == 1 {
+		// One nibble, initialisation
+	}
+
+
 	n := 0
 	blen := len(o.buf) / 3
 	for len(data) != 0 {
@@ -57,13 +64,13 @@ func (o *Bitbang) Write(data []byte) (int, error) {
 			k += 3
 		}
 		k, err := o.w.Write(o.buf[:k])
-		n += k / 3
+		n += k / 4
 		if err != nil {
 			return n, err
 		}
 		data = data[l:]
 	}
-	time.Sleep(16 * time.Millisecond)
+	time.Sleep(100 * time.Millisecond)
 	return n, nil
 }
 

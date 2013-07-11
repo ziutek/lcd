b1f9b123ebacf47f72c40a5ed759ef162548d296
Michał Derkacz <michal@Lnet.pl>
2013-07-11 19:12:19 +0200

    Screen an Area definitions completed

--- screen.go
+++ screen.go
@@ -1,18 +1,40 @@
 package lcd
 
-type TextArea interface {
+import (
+	"image"
+)
+
+type Area interface {
 	Show()
 	Hide()
 	Raise()
-	SetGeometry(x, y, width, height int)
+	Delete()
+	// Move changes area position on the screen.
+	Move(image.Point)
+	// Resize changes area dimensions
+	Resize(image.Point)
+}
+
+type ImageArea interface {
+	Area
+	Update(image.Image)
+	SyncUpdate(image.Image)
+}
+
+type TextArea interface {
+	Area
 	Update([]rune)
 	SyncUpdate([]rune)
-	Delete()
+}
+
+type ImageScreen interface {
+	// NewImageArea creates new image area on the screen.
+	NewImageArea(image.Rectangle) ImageArea
 }
 
 type TextScreen interface {
-	// NewTextArea creates new text area of size (width, height) begins from
-	// x column and y row. Created area is hidden and initialized using
-	// transparent runes (rune(0) means transparent).
-	NewTextArea(x, y, width, height int, sync bool) TextArea
+	// NewTextArea creates new text area
+	// pos contains image.Position on the screen (in screen cordinates), size
+	// contains size of area in area coordinates (runes).
+	NewTextArea(pos, size image.Point) TextArea
 }
